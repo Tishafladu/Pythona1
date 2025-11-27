480583ac2c703489d026eb67d546df43a7fa6b1d
Tisha Faldu <faldutisha95@gmail.com>
2025-11-28 01:21:17 +0530

    Add files via upload

---
+++ PersonalizedGreeting.py
@@ -0,0 +1,6 @@
+first_name = input("Enter the first name: ")
+last_name = input("Enter the last name: ")
+
+full_name = first_name + " " + last_name
+
+print("Hello,", full_name + "! Welcome to the Python programming world.")
