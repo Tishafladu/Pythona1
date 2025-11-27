2cf4068763e571b8bdc0d94e9236e578410035ac
Tisha Faldu <faldutisha95@gmail.com>
2025-11-28 01:20:46 +0530

    Add files via upload

---
+++ index.py
@@ -0,0 +1,14 @@
+n1 = input("Enter the first number: ")
+n2 = input("Enter the second number: ")
+
+a = int(n1)+int(n2)
+print("Addition :",a)
+
+a = int(n1)-int(n2)
+print("Subtration :",a)
+
+a = int(n1)*int(n2)
+print("Multiplication :",a)
+
+a = int(n1)/int(n2)
+print("Division :",a)
